9f6d383b07c8bf24f917ab7d157123c5356ddac1
Ivam <jerryoldson@gmail.com>
2021-07-21 00:37:02 -0300

    a

--- bitch-slap.js
+++ bitch-slap.js
@@ -1,7 +1,6 @@
 function bitchslap() {
 	document.getElementById('bitch').src = "bitch-slap.gif";
-	var elementosClaseCambiar = document.getElementsByClassName('cambiar');
-	for (var i = 0; i < elementosClaseCambiar.length; i++) {
+	for (var i = 0; i < document.getElementsByClassName('cambiar').length; i++) {
 		document.getElementsByClassName('cambiar')[i].innerHTML = "¡¡¡TOMA BITCH SLAP!!!";
 	}
 }
