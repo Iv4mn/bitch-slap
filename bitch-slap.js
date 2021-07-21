168354c901e7e34c44b5cb088bae830f1a972c62
Ivam <jerryoldson@gmail.com>
2021-07-21 00:27:48 -0300

    Cambiando el texto a cambiar

--- bitch-slap.js
+++ bitch-slap.js
@@ -2,7 +2,6 @@ function bitchslap() {
 	document.getElementById('bitch').src = "bitch-slap.gif";
 	var elementosClaseCambiar = document.getElementsByClassName('cambiar');
 	for (var i = 0; i < elementosClaseCambiar.length; i++) {
-		document.getElementsByClassName('cambiar')[i].innerHTML = "¡¡¡ERES MALVADO!!!";
+		document.getElementsByClassName('cambiar')[i].innerHTML = "¡¡¡TOMA BITCH SLAP!!!";
 	}
-	alert("Toma bitch-slap por tocar a los gatitos");
 }
