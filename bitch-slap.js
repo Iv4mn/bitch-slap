ac2a96ba5f4ae3c60a08328153ae4f2ae422f8bb
Galletitaw <73406168+LaGalletita@users.noreply.github.com>
2021-07-20 22:47:52 -0300

    Add files via upload

---
+++ bitch-slap.js
@@ -0,0 +1,7 @@
+function bitchslap() {
+	document.getElementById('bitch').src = "bitch-slap.gif";
+	document.getElementsByClassName("cambiar").innerHTML = "no anda esta chotada";
+	alert("Toma bitch-slap por tocar a los gatitos");
+	
+}
+
