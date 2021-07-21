51ed70ba1dbb5448ed31a0a714a8aefcb57c61f1
Rony Stico <ronystico@gmail.com>
2021-07-20 23:08:34 -0400

    agregado for que recorre getElementsByClassName

--- bitch-slap.js
+++ bitch-slap.js
@@ -1,7 +1,8 @@
 function bitchslap() {
 	document.getElementById('bitch').src = "bitch-slap.gif";
-	document.getElementsByClassName("cambiar").innerHTML = "no anda esta chotada";
+	var elementosClaseCambiar = document.getElementsByClassName('cambiar');
+	for (var i = 0; i < elementosClaseCambiar.length; i++) {
+		document.getElementsByClassName('cambiar')[i].innerHTML = "¡¡¡ERES MALVADO!!!";
+	}
 	alert("Toma bitch-slap por tocar a los gatitos");
-	
 }
-
